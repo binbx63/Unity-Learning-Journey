1b30861c66905c18a9ee815e47a77c474d4244e4
Binbx <binbx63@gmail.com>
2026-01-05 16:20:54 +0900

    Add attack animation playback logic; attack handling not yet implemented

---
+++ SimpleRPG_LearningFromSuKi/Assets/Scripts/Weapon/ScytheWeapon.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class ScytheWeapon : Weapon
+{
+    public const string ANIM_PARM_ISATTACK = "IsAttack"; //字符串容易写错，制作为常量
+    private Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Attack();
+        }
+    }
+    public override void Attack()
+    {
+        anim.SetTrigger(ANIM_PARM_ISATTACK);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Enemy")
+        {
+            //TODO
+            print("Trigger with" + other.name);
+        }
+    }
+}
